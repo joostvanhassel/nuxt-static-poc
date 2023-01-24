fa832fffe43b3ce44a34a12a3d51c810598951a6
joostvanhassel <hello@joostvanhassel.nl>
2023-01-24 21:12:24 +0100

    Initial commit

---
+++ pages/[[path]].vue
@@ -0,0 +1,5 @@
+<template>
+  <h2>path</h2>
+
+  <NuxtLink to="/news/some-title">news article</NuxtLink>
+</template>
---
+++ pages/news/[slug].vue
@@ -0,0 +1,19 @@
+<script setup>
+var { data, pending } = await useAsyncData(() => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(Date.now());
+    }, 2000);
+  });
+});
+</script>
+
+<template>
+  <div v-if="!pending">
+    <h2>news</h2>
+    <p>blablabla some news</p>
+    <p>{{ data }}</p>
+  </div>
+
+  <NuxtLink to="/">home</NuxtLink>
+</template>
